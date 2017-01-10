57708182c43c1fcd2d3726c7b96cefe9df46927f
Katsiaryna_Shastakov <Katsiaryna_Shastakova@epam.com>
2017-01-10 10:25:52 +0300

    Working in FF and Chrome draft

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,39 @@
 var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  browserSync = require('browser-sync').create(),
   svgSprite = require('gulp-svg-sprite'),
 	svgmin = require('gulp-svgmin'),
 	cheerio = require('gulp-cheerio');
 
 var settings = {
   assetsDir: './assets/',
-  outDir: './dist/'
+  outDir: './dist/',
+  sassFiles: './assets/sass/**/*.scss'
 }
+
+gulp.task('sass', ["svgSpriteBuild"], function () {
+  return gulp.src(settings.sassFiles)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(settings.outDir + 'css'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+/**
+ * Wait for jade and sass tasks, then launch the browser-sync Server
+ */
+gulp.task('browser-sync', ['sass'], function () {
+  browserSync.init({
+    server: {
+      baseDir: './',
+      index: "index.html"
+    },
+    notify: false
+  });
+});
+
+gulp.task('watch', function () {
+  gulp.watch(settings.sassFiles, ['sass']);
+});
 
 gulp.task('svgSpriteBuild', function () {
   return gulp.src(settings.assetsDir + 'i/icons/*.svg')
@@ -29,20 +56,21 @@ gulp.task('svgSpriteBuild', function () {
     }))
     // build svg sprite
     .pipe(svgSprite({
-      dest: settings.outDir,
       mode: {
         symbol: {
-          dest: "",
-          sprite: "sprite.svg"
-          , render: {
-            css: {
-              dest: 'sprite.css'
-              , template: settings.assetsDir + "sass/templates/_sprite_template.scss"
+          sprite: "sprite.svg",
+          render: {
+            scss: {
+              dest: '../../assets/sass/_sprite.scss',
+              template: settings.assetsDir + "sass/templates/_sprite_template.scss"
             }
           }
         }
       }
-    })).pipe(gulp.dest(settings.outDir));
+    }))
+    .pipe(gulp.dest(settings.outDir));
 });
 
-gulp.task("default", ["svgSpriteBuild"]);
+gulp.task("default", ["svgSpriteBuild", "sass"]);
+
+gulp.task("server", ["browser-sync", "watch"]);
